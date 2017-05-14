8614b00a2006304cf2113afada4467bb966f78a5
Lukas Biewald <lukeab@gmail.com>
2017-05-14 13:45:07 -0700

    fixed some bugs

--- l2k/get_cf_data.py
+++ l2k/get_cf_data.py
@@ -1,11 +1,28 @@
 import pandas as pd
 import click
+import json
+
+import urllib
+
+def download_data(cf_csv):
+    df = pd.read_csv(cf_csv)
+
+    annotation = [json.loads(a) for a in df['annotation']]
+    gt_urls = [a['url'] for a in annotation] # so pythonic...
+    for i, url in enumerate(gt_urls):
+        urllib.urlretrieve (url, str(i)+"_gt.png")
+
+    image_url = [json.loads(a) for a in df['url']]
+    for i, url in enumerate(image_urls):
+        urllib.urlretrieve (url, str(i)+".png")
 
 @click.command()
 @click.argument('cf-csv', type=click.Path(exists=True))
 def get_data(cf_csv):
-    df = pd.read_csv(cf_csv)
-    print(df)
+    download_data(cf_csv)
+
+
+
 
 if __name__ == '__main__':
     get_data()
